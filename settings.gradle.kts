39ca05dbad3447ee10c2e344b22b625dfeb7d84c
lbressler13 <lbressler13@gmail.com>
2022-06-26 15:36:08 -0400

    update to match main

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/7.4.2/userguide/multi_project_builds.html
  */
 
-rootProject.name = "kotlin-util"
-include("kotlin-util")
+rootProject.name = "kotlin-utils"
+include("kotlin-utils")
