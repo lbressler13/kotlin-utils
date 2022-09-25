aaf453a330cd59e8238da72255e19318547cd22f
lbressler13 <lbressler13@gmail.com>
2022-09-25 02:44:43 -0400

    ci: publishing

--- kotlin-utils/build.gradle.kts
+++ kotlin-utils/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     `java-library`
+    `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.5.31"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0" // ktlint
 }
 
-group = "org.example"
+group = "xyz.lbres"
 version = "0.1.0"
 
 repositories {
@@ -14,4 +15,21 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/lbressler13/kotlin-utils")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
