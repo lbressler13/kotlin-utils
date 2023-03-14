e66957a5f49ddd81402c70c5c0468ec47476aafe
lbressler13 <lbressler13@gmail.com>
2023-03-14 00:24:54 -0400

    fix: missing import

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/MultiSetInlineMethodsTest.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/MultiSetInlineMethodsTest.kt
@@ -1,5 +1,6 @@
 package xyz.lbres.kotlinutils.set.multiset
 
+import xyz.lbres.kotlinutils.set.multiset.inline.* // ktlint-disable no-wildcard-imports no-unused-imports
 import kotlin.test.Test
 
 internal class MultiSetInlineMethodsTest {
