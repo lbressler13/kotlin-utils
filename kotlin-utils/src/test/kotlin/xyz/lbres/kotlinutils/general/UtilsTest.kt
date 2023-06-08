3a43b6cf976f931661bbf9850ad1d840447cae36
lbressler13 <lbressler13@gmail.com>
2023-06-08 12:25:04 -0400

    simpleIf function (#28)

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/general/UtilsTest.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/general/UtilsTest.kt
@@ -7,27 +7,28 @@ import kotlin.test.assertEquals
 
 class UtilsTest {
     @Test
-    fun testTernaryIf() {
+    fun testSimpleIf() {
+        // value
         var expected = true
-        var result = ternaryIf(true, true, false)
+        var result = simpleIf(true, true, false)
         assertEquals(expected, result)
 
         expected = false
-        result = ternaryIf(false, true, false)
+        result = simpleIf(false, true, false)
         assertEquals(expected, result)
 
         expected = false
-        result = ternaryIf(true, false, true)
+        result = simpleIf(true, false, true)
         assertEquals(expected, result)
 
         var testInt = 12
         var expectedInt = 1
-        var resultInt = ternaryIf(testInt.isNegative(), -1, 1)
+        var resultInt = simpleIf(testInt.isNegative(), -1, 1)
         assertEquals(expectedInt, resultInt)
 
         testInt = -12
         expectedInt = -1
-        resultInt = ternaryIf(testInt.isNegative(), -1, 1)
+        resultInt = simpleIf(testInt.isNegative(), -1, 1)
         assertEquals(expectedInt, resultInt)
 
         val isPerfectSquare: (Int) -> Boolean = {
@@ -38,12 +39,44 @@ class UtilsTest {
 
         testInt = 49
         expectedInt = 49
-        resultInt = ternaryIf(isPerfectSquare(testInt), testInt, -1)
+        resultInt = simpleIf(isPerfectSquare(testInt), testInt, -1)
         assertEquals(expectedInt, resultInt)
 
         testInt = 50
         expectedInt = -1
-        resultInt = ternaryIf(isPerfectSquare(testInt), testInt, -1)
+        resultInt = simpleIf(isPerfectSquare(testInt), testInt, -1)
         assertEquals(expectedInt, resultInt)
+
+        // function
+        expected = false
+        result = simpleIf(false, { true }, { false })
+        assertEquals(expected, result)
+
+        val expectedString = ""
+        val string = "a"
+        val resultString = simpleIf(string.length > 1, { string[1] }, "")
+        assertEquals(expectedString, resultString)
+
+        val list = listOf(1, 2, 3)
+
+        expectedInt = 2
+        resultInt = simpleIf(3 in list, list.indexOf(3), 0)
+        assertEquals(expectedInt, resultInt)
+
+        val numerator = 4
+        var denominator = 0
+        expectedInt = 0
+        resultInt = simpleIf(denominator == 0, { 0 }, { numerator / denominator })
+        assertEquals(expectedInt, resultInt)
+
+        denominator = 2
+        expectedInt = 2
+        resultInt = simpleIf(denominator == 0, { 0 }, { numerator / denominator })
+        assertEquals(expectedInt, resultInt)
+
+        // no return
+        var count = 0
+        simpleIf(false, { count += 1 }, { count += 2 })
+        assertEquals(2, count)
     }
 }
