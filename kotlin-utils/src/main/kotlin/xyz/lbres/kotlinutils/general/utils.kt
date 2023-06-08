5e1a989fc668f864e2da7c62371457f6210e9ad1
lbressler13 <lbressler13@gmail.com>
2023-06-08 12:22:57 -0400

    test: additional fn tests

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/general/utils.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/general/utils.kt
@@ -18,12 +18,12 @@ fun <T> simpleIf(check: Boolean, trueValue: T, falseValue: T): T {
 }
 
 /**
- * Function to perform a simple boolean check, and return a value based on the result
- * Computation of the result is delayed until after the check occurs.
+ * Function to perform a simple boolean check, and execute a function based on value of the check.
+ * Returns the value of the function, which can be [Unit].
  *
  * @param check [Boolean]
- * @param trueMethod [T]: method to retrieve value if [check] is `true`
- * @param trueMethod [T]: method to retrieve value if [check] is `false`
+ * @param trueMethod [T]: function to invoke if [check] is `true`
+ * @param trueMethod [T]: function to invoke if [check] is `false`
  * @return result of [trueMethod] if the check is true, result of [falseMethod] otherwise
  */
 fun <T> simpleIf(check: Boolean, trueMethod: () -> T, falseMethod: () -> T): T {
@@ -35,7 +35,7 @@ fun <T> simpleIf(check: Boolean, trueMethod: () -> T, falseMethod: () -> T): T {
 }
 
 /**
- * Function to perform a simple boolean check, and return a number based on the result
+ * Function to perform a simple boolean check, and return a value based on the result
  *
  * @param check [Boolean]
  * @param trueValue [T]: value to return if [check] is true
--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/general/UtilsTest.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/general/UtilsTest.kt
@@ -47,7 +47,7 @@ class UtilsTest {
         resultInt = simpleIf(isPerfectSquare(testInt), testInt, -1)
         assertEquals(expectedInt, resultInt)
 
-        // method
+        // function
         expected = false
         result = simpleIf(false, { true }, { false })
         assertEquals(expected, result)
@@ -63,6 +63,18 @@ class UtilsTest {
         resultInt = simpleIf(3 in list, list.indexOf(3), 0)
         assertEquals(expectedInt, resultInt)
 
+        val numerator = 4
+        var denominator = 0
+        expectedInt = 0
+        resultInt = simpleIf(denominator == 0, { 0 }, { numerator / denominator })
+        assertEquals(expectedInt, resultInt)
+
+        denominator = 2
+        expectedInt = 2
+        resultInt = simpleIf(denominator == 0, { 0 }, { numerator / denominator })
+        assertEquals(expectedInt, resultInt)
+
+        // no return
         var count = 0
         simpleIf(false, { count += 1 }, { count += 2 })
         assertEquals(2, count)
