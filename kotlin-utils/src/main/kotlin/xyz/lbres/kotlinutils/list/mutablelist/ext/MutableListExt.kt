4bb43cd64fb8e2633637480c9b09abeaa331041e
lbressler13 <lbressler13@gmail.com>
2023-06-11 22:33:43 -0400

    feat: setAllValues for MutableList

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/list/mutablelist/ext/MutableListExt.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/list/mutablelist/ext/MutableListExt.kt
@@ -8,3 +8,12 @@ import xyz.lbres.kotlinutils.collection.mutable.ext.popRandom
  * @return [T]?: an element from the list, or `null` if the list is empty
  */
 fun <T> MutableList<T>.popRandom(): T? = popRandom()
+
+/**
+ * Assign all elements to have a given value
+ *
+ * @param value [T]: value to assign
+ */
+fun <T> MutableList<T>.setAllValues(value: T) {
+    indices.forEach { set(it, value) }
+}
