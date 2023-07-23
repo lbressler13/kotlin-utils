9473987b9e63a3f0b38904fb0cc7310f3512b409
lbressler13 <lbressler13@gmail.com>
2023-07-23 11:38:56 -0400

    test: additional unit test

--- kotlinutils/src/test/kotlin/xyz/lbres/kotlinutils/test/TestUtilsTest.kt
+++ kotlinutils/src/test/kotlin/xyz/lbres/kotlinutils/test/TestUtilsTest.kt
@@ -1,5 +1,6 @@
 package xyz.lbres.kotlinutils.test
 
+import xyz.lbres.kotlinutils.set.multiset.multiSetOf
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -13,7 +14,44 @@ class TestUtilsTest {
 
     @Test
     fun testAssertEqualsAnyOf() {
-        // TODO
+        // true
+        var intValue = 5
+        var intOptions: Collection<Int> = listOf(5)
+        assertEqualsAnyOf(intOptions, intValue)
+
+        intOptions = listOf(5, 5, 5, 5, 5)
+        assertEqualsAnyOf(intOptions, intValue)
+
+        intOptions = listOf(15, 20, 23, -8, 2, 5, 4)
+        assertEqualsAnyOf(intOptions, intValue)
+
+        intValue = -8
+        assertEqualsAnyOf(intOptions, intValue)
+
+        var stringValue = "hello world"
+        var stringOptions = setOf("hello", "world", "hello world", "farewell planet")
+        assertEqualsAnyOf(stringOptions, stringValue)
+
+        var setValue = setOf(5, "hello", 'c', 1.5)
+        var setOptions = multiSetOf(setOf(5, "hello", 'c', 1.5), setOf(5, "hello", 'c', 1.5), setOf(5, 1.5))
+        assertEqualsAnyOf(setOptions, setValue)
+
+        // false
+        intValue = 1
+
+        intOptions = setOf(-1)
+        assertFailsWith<AssertionError> { assertEqualsAnyOf(intOptions, intValue) }
+
+        intOptions = setOf(0, 0, 4, 5, 2)
+        assertFailsWith<AssertionError> { assertEqualsAnyOf(intOptions, intValue) }
+
+        stringValue = "hello world"
+        stringOptions = setOf("hello", "world", "Hello World", "farewell planet")
+        assertFailsWith<AssertionError> { assertEqualsAnyOf(stringOptions, stringValue) }
+
+        setValue = setOf(5, "hello", 'c', 1.5)
+        setOptions = multiSetOf(setOf(5, "hello", 'c'), setOf("hello", 'c', 1.5), setOf('c', 1.5, "hello"))
+        assertFailsWith<AssertionError> { assertEqualsAnyOf(setOptions, setValue) }
     }
 
     @Test
@@ -21,9 +59,16 @@ class TestUtilsTest {
         var iter = 0
 
         // first success
-        var test: () -> Unit = {
+        var test: () -> Unit = { iter++ }
+        runTestWithRetry(test)
+        assertEquals(1, iter)
+
+        iter = 0
+        test = {
             iter++
-            val a = 52
+            try {
+                assertTrue(false)
+            } catch (_: AssertionError) {}
         }
         runTestWithRetry(test)
         assertEquals(1, iter)
