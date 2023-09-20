81b199673576f611b4f42aec5ae7f78a493ac1d1
lbressler13 <lbressler13@gmail.com>
2023-09-19 23:17:35 -0400

    test: constructor tests

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/manager/ConstMultiSetManagerTest.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/manager/ConstMultiSetManagerTest.kt
@@ -1,25 +1,27 @@
 package xyz.lbres.kotlinutils.set.multiset.manager
 
 import xyz.lbres.kotlinutils.list.IntList
-import xyz.lbres.kotlinutils.set.multiset.ConstMultiSet
-import xyz.lbres.kotlinutils.set.multiset.ConstMutableMultiSet
-import xyz.lbres.kotlinutils.set.multiset.constMultiSetOf
-import xyz.lbres.kotlinutils.set.multiset.constMutableMultiSetOf
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 
 class ConstMultiSetManagerTest {
-//    @Test fun testConstructor() = runConstructorTests()
-//    @Test fun testEquals() = runEqualsTests()
-//
+    @Test fun testConstructor() = runConstructorTests()
+
 //    @Test fun testContains() = runContainsTests()
 //    @Test fun testContainsAll() = runContainsAllTests()
-//
+
+//    @Test fun testClear() = runClearTests()
+//    @Test fun testAdd() = runAddTests()
+//    @Test fun testAddAll() = runAddAllTests()
+//    @Test fun testRemove() = runRemoveTests()
+//    @Test fun testRemoveAll() = runRemoveAllTests()
+//    @Test fun testRetainAll() = runRetainAllTests()
+
 //    @Test fun testMinus() = runMinusTests()
 //    @Test fun testPlus() = runPlusTests()
 //    @Test fun testIntersect() = runIntersectTests()
-//
+
     @Test fun testIsEmpty() = runIsEmptyTests()
     @Test fun testGetCountOf() = runGetCountOfTests()
 
@@ -111,30 +113,4 @@ class ConstMultiSetManagerTest {
         expected = "[[1, 2, 3], [0, 5, 7]]"
         assertEquals(expected, listManager.toString())
     }
-
-//    @Test
-//    fun testToString() {
-//        var set: ConstMultiSet<Int> = constMultiSetOf()
-//        var expected = "[]"
-//        assertEquals(expected, set.toString())
-//
-//        set = constMultiSetOf(100, -200, 400, 800)
-//        expected = "[100, -200, 400, 800]"
-//        assertEquals(expected, set.toString())
-//
-//        set = constMultiSetOf(1, 1, 1, 1)
-//        expected = "[1, 1, 1, 1]"
-//        assertEquals(expected, set.toString())
-//
-//        set = constMultiSetOf(2, 4, 2, 1)
-//        expected = "[2, 2, 4, 1]"
-//        assertEquals(expected, set.toString())
-//
-//        val list1 = listOf(1, 2, 3)
-//        val list2 = listOf(0, 5, 7)
-//        val listSet: ConstMultiSet<IntList> = constMultiSetOf(list1, list2)
-//
-//        expected = "[[1, 2, 3], [0, 5, 7]]"
-//        assertEquals(expected, listSet.toString())
-//    }
 }
