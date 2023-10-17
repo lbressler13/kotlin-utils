16f0b2929e16895355c7757ffb7c4d4a10deb759
lbressler13 <lbressler13@gmail.com>
2023-10-16 20:47:19 -0400

    feat: list elements equal

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/list/ext/ListExt.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/list/ext/ListExt.kt
@@ -1,5 +1,7 @@
 package xyz.lbres.kotlinutils.list.ext
 
+import xyz.lbres.kotlinutils.set.multiset.const.ConstMultiSetImpl
+
 /**
  * Create a copy of a list, with one value changed
  *
@@ -49,4 +51,12 @@ fun <T> List<T>.copyWithoutLast(): List<T> = subList(0, lastIndex)
  */
 fun <T> List<T>.isSingleValue(): Boolean = size == 1
 
-// TODO: elements equal using const multiset
+/**
+ * Determine if this list contains the same elements as another list, in any order
+ *
+ * @param other [List]<E>: list to check equality with
+ * @return [Boolean] `true` if the lists contain the same elements, `false` otherwise
+ */
+fun <E> List<E>.elementsEqual(other: List<E>): Boolean {
+    return ConstMultiSetImpl(this) == ConstMultiSetImpl(other)
+}
