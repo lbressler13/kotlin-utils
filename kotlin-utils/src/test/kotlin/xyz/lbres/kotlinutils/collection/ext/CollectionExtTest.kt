c339a00dd62ec2371599296301ca79fb68c3c646
lbressler13 <lbressler13@gmail.com>
2023-10-21 16:02:40 -0400

    feat: mapFilter function

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/collection/ext/CollectionExtTest.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/collection/ext/CollectionExtTest.kt
@@ -1,6 +1,10 @@
 package xyz.lbres.kotlinutils.collection.ext
 
+import xyz.lbres.kotlinutils.assertEmpty
+import xyz.lbres.kotlinutils.int.ext.isZero
 import xyz.lbres.kotlinutils.internal.constants.Suppressions
+import xyz.lbres.kotlinutils.list.IntList
+import xyz.lbres.kotlinutils.list.StringList
 import xyz.lbres.kotlinutils.set.multiset.MultiSet
 import xyz.lbres.kotlinutils.set.multiset.MutableMultiSet
 import xyz.lbres.kotlinutils.set.multiset.const.ConstMultiSet
@@ -11,6 +15,8 @@ import xyz.lbres.kotlinutils.set.multiset.const.emptyConstMultiSet
 import xyz.lbres.kotlinutils.set.multiset.emptyMultiSet
 import xyz.lbres.kotlinutils.set.multiset.multiSetOf
 import xyz.lbres.kotlinutils.set.multiset.mutableMultiSetOf
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -184,5 +190,66 @@ class CollectionExtTest {
 
         collection = listOf(1, 2, 3)
         assertTrue(collection.isNotNullOrEmpty())
+    }
+
+    @Test
+    fun testMapFilter() {
+        val multiSet1 = multiSetOf(1, 2, 3, 4, 7, 2, 4, 1)
+        val multiSet2 = multiSetOf(-1, 3, 1, 1, 1, 5, 7, 1, 8, 2)
+        val multiSetMin: (Int) -> Int = { min(multiSet1.getCountOf(it), multiSet2.getCountOf(it)) }
+        val multiSetMax: (Int) -> Int = { max(multiSet1.getCountOf(it), multiSet2.getCountOf(it)) }
+
+        // empty
+        var intCollection: Collection<Int> = emptySet()
+        var intResult = intCollection.mapFilter({ it }, { it > 0 })
+        assertEmpty(intResult)
+
+        // none filtered
+        intCollection = listOf(2, 3, 5, 7, 8)
+        var intExpected = listOf(1, 2, 4, 6, 7)
+        intResult = intCollection.mapFilter({ it - 1 }) {
+            val div = it / 5
+            div * 5 != it
+        }
+        assertEquals(intExpected, intResult)
+
+        var listCollection = listOf(listOf("hello", "world"), listOf("planet", "farewell"), listOf("goodbye", "earth"))
+        var stringExpected = listOf("hello", "planet", "goodbye")
+        var stringResult = listCollection.mapFilter({ it.first() }) { it.length >= 5 }
+        assertEquals(stringExpected, stringResult)
+
+        intCollection = listOf(1, 3, 2)
+        intExpected = listOf(2, 1, 1)
+        intResult = intCollection.mapFilter(multiSetMin) { it > 0 }
+        assertEquals(intExpected, intResult)
+
+        // all filtered
+        intCollection = listOf(2, 3, 5, 7, 8)
+        intResult = intCollection.mapFilter({ it - 1 }) {
+            val div = it / 5
+            div * 5 == it
+        }
+        assertEmpty(intResult)
+
+        listCollection = listOf(listOf("greetings", "world"), listOf("planet", "farewell"), listOf("goodbye", "earth"))
+        stringResult = listCollection.mapFilter({ it.first() }) { it.length <= 5 }
+        assertEmpty(stringResult)
+
+        intCollection = listOf(1, 3, 2, 4, -1, 1)
+        intResult = intCollection.mapFilter(multiSetMax, Int::isZero)
+        assertEmpty(intResult)
+
+        // some filtered
+        intCollection = listOf(1, 3, 4, 2, 1)
+        intExpected = listOf(2, 1, 1, 2)
+        intResult = intCollection.mapFilter(multiSetMin) { it > 0 }
+        assertEquals(intExpected, intResult)
+
+        listCollection = listOf(emptyList(), listOf("123", "abc"), listOf("hello", "12"), listOf("-4"))
+        intExpected = listOf(123, -4)
+        val getIntValue: (StringList) -> Int? = { it.firstOrNull()?.toIntOrNull() }
+        @Suppress(Suppressions.UNCHECKED_CAST)
+        intResult = listCollection.mapFilter(getIntValue) { it != null } as IntList
+        assertEquals(intExpected, intResult)
     }
 }
