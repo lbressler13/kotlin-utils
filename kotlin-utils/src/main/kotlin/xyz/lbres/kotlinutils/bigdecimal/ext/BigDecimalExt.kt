0d01a540e61d0ab1192d87731731ec756326e355
lbressler13 <lbressler13@gmail.com>
2024-03-15 08:13:23 -0400

    BigDecimal methods for isWhole and roundToBigInteger (#62)

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/bigdecimal/ext/BigDecimalExt.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/bigdecimal/ext/BigDecimalExt.kt
@@ -1,8 +1,11 @@
 package xyz.lbres.kotlinutils.bigdecimal.ext
 
 import xyz.lbres.kotlinutils.biginteger.ext.isZero
+import xyz.lbres.kotlinutils.general.succeeds
 import xyz.lbres.kotlinutils.general.tryOrDefault
 import java.math.BigDecimal
+import java.math.BigInteger
+import java.math.RoundingMode
 
 /**
  * Unary check to determine if value is zero
@@ -17,3 +20,20 @@ fun BigDecimal.isZero(): Boolean = tryOrDefault(false) { toBigIntegerExact().isZ
  * @return [Boolean]: true if value is less than zero, false otherwise
  */
 fun BigDecimal.isNegative(): Boolean = this < BigDecimal.ZERO
+
+/**
+ * Determine if value is a whole number
+ *
+ * @return `true` if number is a whole number, `false` otherwise
+ */
+fun BigDecimal.isWholeNumber(): Boolean = succeeds { toBigIntegerExact() }
+
+/**
+ * Round to the nearest whole number using the provided rounding mode
+ *
+ * @param roundingMode [RoundingMode]: mode to use when rounding. Defaults to [RoundingMode.HALF_UP]
+ * @return [BigInteger]: closest whole number by the specified mode
+ */
+fun BigDecimal.roundToBigInteger(roundingMode: RoundingMode = RoundingMode.HALF_UP): BigInteger {
+    return setScale(0, roundingMode).toBigInteger()
+}
