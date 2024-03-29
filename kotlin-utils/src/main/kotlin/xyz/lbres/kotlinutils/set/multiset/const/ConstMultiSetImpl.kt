37374c3e09fb1910cbb7bfdfacca90ff10417a18
lbressler13 <lbressler13@gmail.com>
2024-03-28 22:52:32 -0400

    refactor: ms classes refactor

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/set/multiset/const/ConstMultiSetImpl.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/set/multiset/const/ConstMultiSetImpl.kt
@@ -1,4 +1,33 @@
 package xyz.lbres.kotlinutils.set.multiset.const
 
+import xyz.lbres.kotlinutils.generic.ext.ifNull
+import xyz.lbres.kotlinutils.set.multiset.MultiSet
+import xyz.lbres.kotlinutils.set.multiset.utils.CountsMap
+
 // final implementation of ConstMultiSet
-internal class ConstMultiSetImpl<E>(initialElements: Collection<E>) : ConstMultiSet<E>(initialElements)
+internal class ConstMultiSetImpl<E>(private val elements: Collection<E>, initialCounts: CountsMap<E>? = null) : ConstMultiSet<E>(), AbstractConstMultiSetImpl<E> {
+    override val size: Int = elements.size
+    override val distinctValues: Set<E>
+    private val string: String
+    override val counts: CountsMap<E>
+
+    init {
+        counts = initialCounts.ifNull { CountsMap.from(elements) }
+        distinctValues = counts.distinct
+        string = counts.toString()
+    }
+
+    override fun getCountOf(element: E): Int = counts.getCountOf(element)
+    override fun contains(element: E): Boolean = counts.contains(element)
+    override fun containsAll(elements: Collection<E>): Boolean = counts.containsAll(elements)
+
+    override fun plus(other: MultiSet<E>): MultiSet<E> = super<AbstractConstMultiSetImpl>.plus(other)
+    override fun minus(other: MultiSet<E>): MultiSet<E> = super<AbstractConstMultiSetImpl>.minus(other)
+    override fun intersect(other: MultiSet<E>): MultiSet<E> = super<AbstractConstMultiSetImpl>.intersect(other)
+
+    override fun isEmpty(): Boolean = counts.isEmpty()
+    override fun iterator(): Iterator<E> = elements.iterator()
+    override fun hashCode(): Int = getHashCode()
+    override fun equals(other: Any?): Boolean = other is MultiSet<*> && equalsSet(other)
+    override fun toString(): String = string
+}
--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/utils/UtilsTest.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/utils/UtilsTest.kt
@@ -1,94 +1,121 @@
 package xyz.lbres.kotlinutils.set.multiset.utils
 
+import xyz.lbres.kotlinutils.general.simpleIf
+import xyz.lbres.kotlinutils.int.ext.isZero
+import xyz.lbres.kotlinutils.set.multiset.MultiSet
+import xyz.lbres.kotlinutils.set.multiset.const.ConstMultiSet
+import xyz.lbres.kotlinutils.set.multiset.emptyMultiSet
 import xyz.lbres.kotlinutils.set.multiset.multiSetOf
+import kotlin.math.min
 import kotlin.test.Test
-import kotlin.test.assertContains
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertIsNot
 
 class UtilsTest {
     @Test
-    fun testCreateCountsMap() {
-        var intValues: Collection<Int> = emptyList()
-        assertEquals(emptyMap(), createCountsMap(intValues))
+    fun testCombineCounts() {
+        // empty
+        var intCounts: CountsMap<Int> = CountsMap(emptyMap())
+        var intSet: MultiSet<Int> = emptyMultiSet()
+        var intResult = combineCounts(intCounts, intSet, Int::minus, false, false)
+        assertEquals(emptyMultiSet(), intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        intValues = setOf(1)
-        assertEquals(mapOf(1 to 1), createCountsMap(intValues))
+        intCounts = CountsMap.from(listOf(1, 4, 1, 1))
+        intSet = emptyMultiSet()
+        var intExpected = multiSetOf(1, 1, 1, 4)
+        intResult = combineCounts(intCounts, intSet, Int::minus, false, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        intValues = listOf(1, 1, 1)
-        assertEquals(mapOf(1 to 3), createCountsMap(intValues))
+        // constant map function
+        intCounts = CountsMap.from(listOf(1, 4, 5, 1, 2))
+        intSet = multiSetOf(1, 2, -4, 5, 5)
+        var map: (Int, Int) -> Int = { _, _ -> 0 }
+        intExpected = emptyMultiSet()
+        intResult = combineCounts(intCounts, intSet, map, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        intValues = multiSetOf(1, 1, 4, 1, 2, 4)
-        assertEquals(mapOf(1 to 3, 2 to 1, 4 to 2), createCountsMap(intValues))
+        intCounts = CountsMap.from(listOf(1, 4, 5, 1, 2))
+        intSet = multiSetOf(1, 2, -4, 5, 5)
+        map = { _, _ -> -1 }
+        intExpected = emptyMultiSet()
+        intResult = combineCounts(intCounts, intSet, map, true, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        val stringValues = listOf("hello", "", "world", "", "hello world", "", "world", "")
-        val stringExpected = mapOf("hello" to 1, "world" to 2, "hello world" to 1, "" to 4)
-        assertEquals(stringExpected, createCountsMap(stringValues))
+        intCounts = CountsMap.from(listOf(1, 4, 5, 1, 2))
+        intSet = multiSetOf(1, 2, -4, 5, 5)
+        map = { _, _ -> 2 }
+        intExpected = multiSetOf(-4, -4, 1, 1, 2, 2, 4, 4, 5, 5)
+        intResult = combineCounts(intCounts, intSet, map, true, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        val listValues = listOf(listOf(1, 2, 3), listOf(1, 2, 3), listOf("abc"), listOf("a", "b", "c"))
-        val listExpected = mapOf(listOf(1, 2, 3) to 2, listOf("abc") to 1, listOf("a", "b", "c") to 1)
-        assertEquals(listExpected, createCountsMap(listValues))
-    }
+        // dynamic map function
+        intCounts = CountsMap.from(listOf(1, 4, 5, 1, 2))
+        intSet = multiSetOf(1, 2, -4, 5, 5)
+        intExpected = multiSetOf(1, 1, 1, 2, 2, 4, 5, 5, 5)
+        intResult = combineCounts(intCounts, intSet, Int::plus, false, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-    @Test
-    fun testCountsToString() {
-        var intCounts: Map<Int, Int> = emptyMap()
-        assertEquals("[]", countsToString(intCounts))
+        intCounts = CountsMap.from(listOf(1, 4, 4, 4, -4, 5))
+        intSet = multiSetOf(1, 1, 5, 4, 4)
+        intExpected = multiSetOf(1, 1, 4, 4, 4, 4, 4, 4, 5)
+        intResult = combineCounts(intCounts, intSet, Int::times, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        intCounts = mapOf(4 to 1)
-        assertEquals("[4]", countsToString(intCounts))
+        // use all values
+        intCounts = CountsMap.from(listOf(1, 4, 5, 1, 2))
+        intSet = multiSetOf(1, 2, -4, 5, 5)
+        intExpected = multiSetOf(-4, 1, 1, 1, 2, 2, 4, 5, 5, 5)
+        intResult = combineCounts(intCounts, intSet, Int::plus, true)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        intCounts = mapOf(4 to 3)
-        assertEquals("[4, 4, 4]", countsToString(intCounts))
+        intCounts = CountsMap(mapOf(7 to 6, 4 to 1, 1 to 1, 8 to 3))
+        intSet = multiSetOf(6, 6, 7, 1, 8, 8, 7, 12, 1)
+        map = { i1, i2 -> simpleIf(i1.isZero(), i2, min(i1, i2)) }
+        intExpected = multiSetOf(7, 7, 6, 6, 8, 8, 1, 12)
+        intResult = combineCounts(intCounts, intSet, map, true, false)
+        assertEquals(intExpected, intResult)
+        assertIsNot<ConstMultiSet<*>>(intResult)
 
-        intCounts = mapOf(3 to 1, 1 to 2, 2 to 1)
-        var options = setOf(
-            "[1, 1, 2, 3]", "[1, 1, 3, 2]", "[2, 1, 1, 3]", "[2, 3, 1, 1]", "[3, 1, 1, 2]", "[3, 2, 1, 1]"
-        )
-        assertContains(options, countsToString(intCounts))
+        // const
+        intCounts = CountsMap.from(listOf(1, 4, 4, 4, -4, 5))
+        intSet = multiSetOf(1, 1, 5, 4, 4)
+        intExpected = multiSetOf(1, 1, 4, 4, 4, 4, 4, 4, 5)
+        intResult = combineCounts(intCounts, intSet, Int::times, false, true)
+        assertEquals(intExpected, intResult)
+        assertIs<ConstMultiSet<*>>(intResult)
 
-        var stringCounts = mapOf("abc" to 4, "hello" to 2, "world" to 3)
-        options = setOf(
-            "[abc, abc, abc, abc, hello, hello, world, world, world]",
-            "[abc, abc, abc, abc, world, world, world, hello, hello]",
-            "[hello, hello, abc, abc, abc, abc, world, world, world]",
-            "[hello, hello, world, world, world, abc, abc, abc, abc]",
-            "[world, world, world, abc, abc, abc, abc, hello, hello]",
-            "[world, world, world, hello, hello, abc, abc, abc, abc]"
-        )
-        assertContains(options, countsToString(stringCounts))
+        intCounts = CountsMap.from(listOf(1, 4, 5, 1, 2))
+        intSet = multiSetOf(1, 2, -4, 5, 5)
+        intExpected = multiSetOf(1, 1, 1, 2, 2, 4, 5, 5, 5)
+        intResult = combineCounts(intCounts, intSet, Int::plus, false, true)
+        assertEquals(intExpected, intResult)
+        assertIs<ConstMultiSet<*>>(intResult)
 
-        stringCounts = mapOf("" to 6)
-        assertEquals("[, , , , , ]", countsToString(stringCounts))
+        // other types
+        val stringCounts = CountsMap.from(listOf("hello", "world", "hello world", "world", "world", "hello"))
+        val stringSet = multiSetOf("world", "world", "planet", "hello world")
+        val stringExpected = multiSetOf("world", "world", "hello world")
+        val stringResult = combineCounts(stringCounts, stringSet, ::min, true, true)
+        assertEquals(stringExpected, stringResult)
+        assertIs<ConstMultiSet<*>>(stringResult)
 
-        val listCounts = mapOf(listOf(1, 2, 3) to 2, listOf("hello") to 1)
-        options = setOf("[[1, 2, 3], [1, 2, 3], [hello]]", "[[hello], [1, 2, 3], [1, 2, 3]]")
-        assertContains(options, countsToString(listCounts))
-    }
-
-    @Test
-    fun testCountsToList() {
-        var intCounts: Map<Int, Int> = emptyMap()
-        assertEquals(emptyList(), countsToList(intCounts))
-
-        intCounts = mapOf(2 to 3)
-        assertEquals(listOf(2, 2, 2), countsToList(intCounts))
-
-        intCounts = mapOf(2 to 0, 0 to 2)
-        assertEquals(listOf(0, 0), countsToList(intCounts))
-
-        intCounts = mapOf(1 to 2, 8 to 1, -4 to 3)
-        val intOptions = setOf(
-            listOf(1, 1, 8, -4, -4, -4),
-            listOf(1, 1, -4, -4, -4, 8),
-            listOf(8, 1, 1, -4, -4, -4),
-            listOf(8, -4, -4, -4, 1, 1),
-            listOf(-4, -4, -4, 1, 1, 8),
-            listOf(-4, -4, -4, 8, 1, 1)
-        )
-        assertContains(intOptions, countsToList(intCounts))
-
-        val stringCounts = mapOf("hello" to 1, "world" to 2)
-        val stringOptions = setOf(listOf("hello", "world", "world"), listOf("world", "world", "hello"))
-        assertContains(stringOptions, countsToList(stringCounts))
+        val map1 = CountsMap(mapOf(7 to 2, 8 to 3))
+        val map2 = CountsMap(mapOf(3 to 2, 1 to 7))
+        val map3 = CountsMap(mapOf(4 to 15, 1 to 3, 4 to 5, -6 to 1))
+        val countsMapCounts = CountsMap.from(listOf(map2, map3, map2, map1, map2, map1, map1, map2, map1, map3))
+        val countsMapSet = multiSetOf(map3, map2, map3, map3, map1, map1, map1, map2)
+        val countsMapExpected = multiSetOf(map1, map2, map2)
+        val countsMapResult = combineCounts(countsMapCounts, countsMapSet, Int::minus, false, false)
+        assertEquals(countsMapExpected, countsMapResult)
+        assertIsNot<ConstMultiSet<*>>(countsMapResult)
     }
 }
