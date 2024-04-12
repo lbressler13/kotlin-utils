9923173a9ceefe3960dd0efe69dae91450ccb374
lbressler13 <lbressler13@gmail.com>
2024-04-11 20:51:15 -0400

    refactor: additional helpers for ms tests

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/inline/mapConsistentTests.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/inline/mapConsistentTests.kt
@@ -1,9 +1,13 @@
 package xyz.lbres.kotlinutils.set.multiset.inline
 
 import xyz.lbres.kotlinutils.general.simpleIf
+import xyz.lbres.kotlinutils.internal.constants.Suppressions
 import xyz.lbres.kotlinutils.list.IntList
 import xyz.lbres.kotlinutils.list.ext.copyWithoutLast
 import xyz.lbres.kotlinutils.set.multiset.* // ktlint-disable no-wildcard-imports no-unused-imports
+import xyz.lbres.kotlinutils.set.multiset.impl.MultiSetImpl
+import xyz.lbres.kotlinutils.set.multiset.testutils.GenericMapFn
+import xyz.lbres.kotlinutils.set.multiset.testutils.runCommonMapToSetConsistentTests
 import java.lang.NullPointerException
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
@@ -115,73 +119,22 @@ fun runMapConsistentTests() {
 }
 
 fun runMapToSetConsistentTests() {
-    var intSet = multiSetOf<Int>()
-    var expectedInt = emptyMultiSet<Int>()
-    assertEquals(expectedInt, intSet.mapToSetConsistent { it * 2 })
-
-    intSet = multiSetOf(1, 1, 5, 7, -3, 0, 2, 5)
-    expectedInt = multiSetOf(-3, 0, 1, 1, 2, 5, 5, 7)
-    assertEquals(expectedInt, intSet.mapToSetConsistent { it })
-
-    expectedInt = multiSetOf(-6, 0, 2, 2, 4, 10, 10, 14)
-    assertEquals(expectedInt, intSet.mapToSetConsistent { it * 2 })
-
-    var expectedString = multiSetOf("-4", "-1", "0", "0", "1", "4", "4", "6")
-    assertEquals(expectedString, intSet.mapToSetConsistent { (it - 1).toString() })
-
-    var stringSet = multiSetOf("hello", "world", "goodbye", "world", "hello", "hi", "world", "wrong")
-    expectedString = multiSetOf("greetings", "planet", "farewell", "planet", "greetings", "greetings", "planet", "leave this planet")
-    assertEquals(expectedString, stringSet.mapToSetConsistent { helloWorldMap(it) })
-
-    var helperString = "1"
-    stringSet = multiSetOf("1", "2", "3", "4", "5")
-    expectedString = multiSetOf("11", "112", "1113", "11114", "111115")
-    val addingMap: (String) -> String = {
-        val result = "$helperString$it"
-        helperString += "1"
-        result
+    val mapFn: GenericMapFn<*, *> = { set, fn ->
+        @Suppress(Suppressions.UNCHECKED_CAST)
+        set.mapToSetConsistent(fn as (Any?) -> Any)
     }
-    assertEquals(expectedString, stringSet.mapToSetConsistent { addingMap(it) })
+    runCommonMapToSetConsistentTests(::MultiSetImpl, mapFn, false)
 
-    expectedString = multiSetOf("", "", "", "", "")
-    assertEquals(expectedString, stringSet.mapToSetConsistent { "" })
-
-    stringSet = multiSetOf("hello", "world", "goodbye", "world", "hello", "hi", "world", "wrong")
-    expectedInt = multiSetOf(1, 1, 1, 2, 2, 3, 3, 3)
-    assertEquals(expectedInt, stringSet.mapToSetConsistent { stringSet.getCountOf(it) })
-
-    val errorSet = multiSetOf(e1, e2, e3)
-    val expectedStringNull = multiSetOf("Cannot cast Int to List", "Cannot invoke method on null value", null)
-    assertEquals(expectedStringNull, errorSet.mapToSetConsistent { it.message })
-
-    var listSet = multiSetOf(listOf(1, 2, 3), listOf(4, 5, 6), emptyList(), listOf(7), listOf(7), listOf(7))
-    val expectedList = multiSetOf(emptyList(), listOf(1, 2), listOf(4, 5), listOf(7), listOf(7), listOf(7))
-    assertEquals(expectedList, listSet.mapToSetConsistent(shortenListMap))
-
-    // modified
-    var modString = ""
-    intSet = multiSetOf(1, 1, 2, 1, 3)
-    val modMap: (Int) -> String = {
-        modString += "1"
-        modString
-    }
-    var resultOptions = listOf(
-        multiSetOf("1", "1", "1", "11", "111"),
-        multiSetOf("1", "11", "11", "11", "111"),
-        multiSetOf("1", "11", "111", "111", "111")
-    )
-    assertContains(resultOptions, intSet.mapToSetConsistent(modMap))
-
-    modString = ""
     val mutableList1 = mutableListOf(1, 2, 3)
     val mutableList2 = mutableListOf(0, 5, 7)
+    var modString = ""
     val modMapList: (IntList) -> String = {
         modString += "1"
         modString
     }
 
-    listSet = multiSetOf(mutableList1, mutableList2, listOf(1, 2, 3))
-    resultOptions = listOf(
+    var listSet = multiSetOf(mutableList1, mutableList2, listOf(1, 2, 3))
+    var resultOptions = listOf(
         multiSetOf("1", "1", "11"),
         multiSetOf("1", "11", "11")
     )
