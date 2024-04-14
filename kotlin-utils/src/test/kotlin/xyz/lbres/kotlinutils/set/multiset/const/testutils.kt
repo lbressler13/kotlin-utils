ffe8f2c52ed82bb9c6a2081c70ada941986e61eb
lbressler13 <lbressler13@gmail.com>
2024-04-14 19:26:16 -0400

    chore: remove default param value

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/const/testutils.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/const/testutils.kt
@@ -3,7 +3,7 @@ package xyz.lbres.kotlinutils.set.multiset.const
 import xyz.lbres.kotlinutils.internal.constants.Suppressions
 
 /**
- * Convert generic create set function to create set function for a specific type
+ * Convert generic create set const function to create const set function for a specific type
  *
  * @param genericCreateSet: generic function
  * @return type-specific function
