9894b59133563385c7167318238d447393171f87
lbressler13 <lbressler13@gmail.com>
2026-06-01 19:20:40 -0400

    fix: correct replacement

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/testutils/commonMinusTests.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/set/multiset/testutils/commonMinusTests.kt
@@ -22,7 +22,7 @@ private val genericConstMinus: MultiSetOp<*> = { set1: MultiSet<Any>, set2: Mult
     set1 as ConstMultiSet<Any>
     set2 as ConstMultiSet<Any>
     // set1 `-c` set2
-    set1.plusC(set2)
+    set1.minusC(set2)
 } as MultiSetOp<*>
 
 fun runMinusTests(createSet: (Collection<*>) -> MultiSet<*>, createOtherSet: (Collection<*>) -> MultiSet<*>, const: Boolean) {
