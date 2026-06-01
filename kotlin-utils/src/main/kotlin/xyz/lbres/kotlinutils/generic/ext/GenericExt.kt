823407c028a0047e4654c61bc400a7cc3f4ed25a
lbressler13 <lbressler13@gmail.com>
2026-06-01 19:54:25 -0400

    feat: ifNotNull

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/generic/ext/GenericExt.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/generic/ext/GenericExt.kt
@@ -21,3 +21,11 @@ fun <T> T?.isNull(): Boolean = this == null
  * @return [Boolean]
  */
 fun <T> T?.isNotNull(): Boolean = this != null
+
+/**
+ * Execute a block of code to generate a return value if this value is not null
+ *
+ * @param block: block of code to execute
+ * @return `null` if this value is `null`, or result of executing [block] on the non-null value
+ */
+fun <T, S> T?.ifNotNull(block: (T) -> S): S? = if (this == null) null else block(this)
