b5ad7e9137377a16e39fdb625a685253a71bf8d0
lbressler13 <lbressler13@gmail.com>
2026-06-02 07:27:34 -0400

    feat: any equals

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/iterable/IterableExt.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/iterable/IterableExt.kt
@@ -37,4 +37,12 @@ fun <S, T> Iterable<S>.forEachWithIndexed(other: Iterable<T>, action: (IndexedVa
  * @param element T: value to match
  * @return [Int]: number of elements with the given value
  */
-fun <T> Iterable<T>.countElement(element: T) = this.count { it == element }
+fun <T> Iterable<T>.countElement(element: T): Int = this.count { it == element }
+
+/**
+ * Check if any elements in the array are equal to the given value
+ *
+ * @param element T: value to match
+ * @return [T]?: `true` if the element is present, `false` otherwise
+ */
+fun <T> Iterable<T>.anyEquals(element: T): Boolean = this.any { it == element }
