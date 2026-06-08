a380eb5c2b4081b6d2b7b6c5b1c04e6b903c6ffc
lbressler13 <lbressler13@gmail.com>
2026-06-07 22:53:54 -0400

    Either and MutableEither (#81)

---
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/utils/either/EitherTest.kt
@@ -0,0 +1,34 @@
+package xyz.lbres.kotlinutils.utils.either
+
+import xyz.lbres.kotlinutils.internal.constants.Suppressions
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@Suppress(Suppressions.REMOVE_EXPLICIT_TYPES)
+class EitherTest {
+    private val mutable = false
+
+    @Test fun testConstructor() = runTestConstructor(mutable)
+    @Test fun testWithLeft() = runTestWithLeft(mutable)
+    @Test fun testWithRight() = runTestWithRight(mutable)
+
+    @Test fun testIsNull() = runTestIsNull(mutable)
+    @Test fun testToMutableEither() = runTestToEither(mutable)
+
+    @Test
+    fun testEquals() {
+        runTestEquals(mutable)
+
+        // mutable
+        val either = Either<Int, List<Int>>(listOf(1, 2, 3))
+        val mutable = MutableEither<List<Int>, Int>(listOf(1, 2, 3))
+        assertTrue(either == mutable)
+        mutable.left = listOf(1)
+        assertFalse(either == mutable)
+    }
+
+    @Test fun testEqualsValue() = runTestEqualsValue(mutable)
+
+    @Test fun testToString() = runTestToString(mutable = mutable)
+}
---
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/utils/either/eitherTestHelpers.kt
@@ -0,0 +1,77 @@
+package xyz.lbres.kotlinutils.utils.either
+
+import xyz.lbres.kotlinutils.internal.constants.Suppressions
+import xyz.lbres.kotlinutils.utils.simpleIf
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+// check that all fields match the expected state when the left value is set to a specific value
+fun <S, T> checkLeft(either: Either<S, T>, value: S, mutable: Boolean? = null) {
+    assertEquals(value, either.left)
+    assertNull(either.right)
+    assertTrue(either.isLeft)
+    assertFalse(either.isRight)
+
+    if (mutable != null) {
+        assertEquals(mutable, either is MutableEither<S, T>)
+    }
+}
+
+// check that all fields match the expected state when the right value is set to a specific value
+fun <S, T> checkRight(either: Either<S, T>, value: T, mutable: Boolean? = null) {
+    assertNull(either.left)
+    assertEquals(value, either.right)
+    assertFalse(either.isLeft)
+    assertTrue(either.isRight)
+
+    if (mutable != null) {
+        assertEquals(mutable, either is MutableEither<S, T>)
+    }
+}
+
+// check both directions of equality
+fun <S, T, U, V> bothEquals(first: Either<S, T>, second: Either<U, V>) {
+    assertTrue(first == second)
+    assertTrue(second == first)
+}
+
+// check both directions of non-equality
+fun <S, T, U, V> bothNotEquals(first: Either<S, T>, second: Either<U, V>) {
+    assertFalse(first == second)
+    assertFalse(second == first)
+}
+
+// construct using left value, taking mutability into account
+fun <S, T> constructL(value: S, mutable: Boolean): Either<S, T> {
+    return simpleIf(mutable, MutableEither(value), Either(value))
+}
+
+// construct using right value, taking mutability into account
+@Suppress(Suppressions.REMOVE_EXPLICIT_TYPES)
+fun <S, T> constructR(value: T, mutable: Boolean): Either<S, T> {
+    return simpleIf(mutable, MutableEither<S, T>(value), Either<S, T>(value))
+}
+
+// set the left value, either by modifying a mutable instance or by generating a new immutable instance
+fun <S, T> setLeft(either: Either<S, T>, value: S, mutable: Boolean): Either<S, T> {
+    return if (mutable) {
+        either as MutableEither<S, T>
+        either.left = value
+        either
+    } else {
+        Either.withLeft(value)
+    }
+}
+
+// set the right value, either by modifying a mutable instance or by generating a new immutable instance
+fun <S, T> setRight(either: Either<S, T>, value: T, mutable: Boolean): Either<S, T> {
+    return if (mutable) {
+        either as MutableEither<S, T>
+        either.right = value
+        either
+    } else {
+        Either.withRight(value)
+    }
+}
