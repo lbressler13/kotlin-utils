9cc5a446305be45df55572db0c827dbde72c9ddc
lbressler13 <lbressler13@gmail.com>
2026-06-09 22:44:01 -0400

    fix: check for empty grep output

--- migrations/v2.sh
+++ migrations/v2.sh
@@ -6,10 +6,16 @@ basePackage="xyz.lbres.kotlinutils"
 
 replacePaths() {
   oldPath="$basePackage.$1"
-  newPath="$basePackage.$n"
+  newPath="$basePackage.$2"
   oldPathE="$(sed -e 's/\./\\./g' <<< "$1")"
   newPathE="$(sed -e 's/\./\\./g' <<< "$2")"
-  git grep -l $oldPathE $rootPath | xargs sed -i "s/$oldPathE/$newPathE/"
+  output=$(git grep -rl $oldPathE $rootPath)
+  if [[ -z $output ]]; then
+    echo "No occurrences of '$oldPath' found, skipping"
+  else
+    echo "Replacing '$oldPath' with '$newPath'"
+    echo $output | xargs sed -i "s/$oldPathE/$newPathE/"
+  fi
 }
 
 # ext
