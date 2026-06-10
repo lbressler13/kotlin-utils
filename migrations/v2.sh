79cbffd5a05f03d757a9f08742930e2e75a6956e
lbressler13 <lbressler13@gmail.com>
2026-06-09 22:13:31 -0400

    feat: script with one replacement

---
+++ migrations/v2.sh
@@ -0,0 +1,8 @@
+
+rootPath=kotlin-utils # TODO param
+
+previousName="xyz.lbres.kotlinutils.classes.labelled.Labelled"
+newName="xyz.lbres.kotlinutils.utils.Labelled"
+previousNameE="$(sed -e 's/\./\\./g' <<< "$previousName")"
+newNameE="$(sed -e 's/\./\\./g' <<< "$newName")"
+git grep -l $previousNameE $rootPath | xargs sed -i "s/$previousNameE/$newNameE/"
