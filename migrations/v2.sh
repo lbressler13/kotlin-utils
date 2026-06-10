ae58e00e4f0a1c694ffb9eded2daf07aba721c44
lbressler13 <lbressler13@gmail.com>
2026-06-09 23:07:42 -0400

    feat: list everything

--- migrations/v2.sh
+++ migrations/v2.sh
@@ -11,14 +11,12 @@ replacePaths() {
   newPathE="$(sed -e 's/\./\\./g' <<< "$2")"
   output=$(git grep -rl $oldPathE $rootPath)
   if [[ -z $output ]]; then
-    echo "No occurrences of '$oldPath' found, skipping"
+    echo "No occurrences of '$oldPath' found skipping"
   else
     echo "Replacing '$oldPath' with '$newPath'"
     echo $output | xargs sed -i "s/$oldPathE/$newPathE/"
   fi
 }
-
-# ext
 
 # deprecated
 declare -A deprecations
@@ -33,10 +31,55 @@ for key in "${!deprecations[@]}"; do
   replacePaths $key $value
 done
 
+# ext
+# TODO
 
-# others
+# utils
+replacePaths "general" "utils"
 
-# bool array/collection come at end
+# numbers
+bigdecimal=("isWholeNumber" "roundToBigInteger")
+for fn in "${bigdecimal[@]}"; do
+  replacePaths "bigdecimal.$fn" "number.bigdecimal.$fn"
+done
 
+numbers=("bigdecimal" "biginteger" "char" "int" "long")
+for p in "${numbers[@]}"; do
+  replacePaths $p "number"
+done
+
+# closedranges
+closedranges=("charrange" "intrange" "longrange")
+for p in "${closedranges[@]}"; do
+  replacePaths "closedrange.$p" "closedrange"
+done
+
+# arrays
+booleanarray=("all" "none" "any")
+for fn in "${booleanarray[@]}"; do
+  replacePaths "array.booleanarray.$fn" "array.booleanarray.$fn"
+done
+
+arrays=("booleanarray" "chararray" "bytearray" "doublearray" "floatarray" "intarray" "longarray" "shortarray")
+for p in "${arrays[@]}"; do
+  replacePaths $p "array"
+done
+
+# collections
+declare -A collection
+collections["collection.boolean"]="collection.bool"
+collections["collection.char"]="collection.number"
+collections["collection.int"]="collection.number"
+collections["collection.long"]="collection.number"
+collections["collection.mutable"]="collection"
+collections["list"]="collection.list"
+collections["map.mutablemap"]="collection.map"
+collections["set.multiset.const"]="collection.multiset" # must be before set.multiset
+collections["set.multiset"]="collection.multiset"
+
+for key in "${!collections[@]}"; do
+  value=${collections[$key]}
+  replacePaths $key $value
+done
 
 # import xyz.lbres.kotlinutils.classes.labelled.Labelled
