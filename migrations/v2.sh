940387fd42fc50a9b54a57109fca4652c226bef2
lbressler13 <lbressler13@gmail.com>
2026-06-09 22:27:23 -0400

    refactor: base package

--- migrations/v2.sh
+++ migrations/v2.sh
@@ -2,10 +2,11 @@
 # TODO snake case?
 
 rootPath="${1:-.}"
+basePackage="xyz.lbres.kotlinutils"
 
 replacePaths() {
-  oldPath=$1
-  newPath=$2
+  oldPath="$basePackage.$1"
+  newPath="$basePackage.$n"
   oldPathE="$(sed -e 's/\./\\./g' <<< "$1")"
   newPathE="$(sed -e 's/\./\\./g' <<< "$2")"
   git grep -l $oldPathE $rootPath | xargs sed -i "s/$oldPathE/$newPathE/"
@@ -19,6 +20,8 @@ replacePaths() {
 
 # bool array/collection come at end
 
-oldName="xyz.lbres.kotlinutils.classes.labelled.Labelled"
-newName="xyz.lbres.kotlinutils.utils.Labelled"
+oldName="classes.labelled.Labelled"
+newName="utils.Labelled"
 replacePaths $oldName $newName
+
+# import xyz.lbres.kotlinutils.classes.labelled.Labelled
