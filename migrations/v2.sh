81a733216eb5e54a2c64e3468284fdfb1fad4ac4
lbressler13 <lbressler13@gmail.com>
2026-06-14 13:46:30 -0400

    docs: add to help

--- migrations/v2.sh
+++ migrations/v2.sh
@@ -21,6 +21,9 @@ printHelpOptions() {
   echo "$tab--src-path: root path for update, defaults to current directory"
   echo "$tab--no-git: run script in a non-git repo, using the grep command instead of git grep. This can cause damage in git repositories."
   echo "$tab--log-skipped: print the names of checks which are being skipped due to no matching files"
+
+  echo
+  echo "Sample usage: ./v2.sh --src-path=\"src\" --log-skipped"
 }
 
 # read input flags
