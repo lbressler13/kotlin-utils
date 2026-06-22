52c1e3ccd719cd3088a3500b9ec6ce90283977e7
lbressler13 <lbressler13@gmail.com>
2026-06-21 22:23:37 -0400

    feat: mapping helper

--- kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/testutils/number/commonMathTests.kt
+++ kotlin-utils/src/test/kotlin/xyz/lbres/kotlinutils/testutils/number/commonMathTests.kt
@@ -26,27 +26,29 @@ inline fun <reified S, T> testProductGeneric(createValues: (List<S>) -> T, cross
 
 @Suppress(Suppressions.UNCHECKED_CAST)
 inline fun <reified S> getSumValues(): List<Pair<List<S>, S>> {
-    val byteValues: List<Pair<List<Byte>, Byte>> = listOf(
-        emptyList<Byte>() to 0,
-        listOf(33).map(Int::toByte) to 33,
-        listOf(-33).map(Int::toByte) to -33,
-        listOf(5, -5).map(Int::toByte) to 0,
-        listOf(-100, 45, -10, 67, -99).map(Int::toByte) to -97,
-        listOf(-100, 45, -10, -67, 99).map(Int::toByte) to -33,
+    val byteValues = listOf(
+        emptyList<Int>() to 0,
+        listOf(33) to 33,
+        listOf(-33) to -33,
+        listOf(5, -5) to 0,
+        listOf(-100, 45, -10, 67, -99) to -97,
+        listOf(-100, 45, -10, -67, 99) to -33,
     )
-    val charValues: List<Pair<List<Char>, Char>> = listOf(
-        emptyList<Char>() to Char(0),
-        listOf(Char(33)) to Char(33),
-        listOf(100, 45, 10, 67, 99).map(Int::toChar) to Char(321),
+    val charValues = listOf(
+        emptyList<Int>() to 0,
+        listOf(33) to 33,
+        listOf(100, 45, 10, 67, 99) to 321,
     )
     val doubleValues: List<Pair<List<Double>, Double>> = listOf()
     val floatValues: List<Pair<List<Float>, Float>> = listOf()
     val intValues: List<Pair<List<Int>, Int>> = listOf()
     val longValues: List<Pair<List<Long>, Long>> = listOf()
-    val shortValues: List<Pair<List<Short>, Short>> = listOf()
+    val shortValues: List<Pair<List<Short>, Short>> = listOf(
+
+    )
     return when (S::class) {
-        Byte::class -> byteValues
-        Char::class -> charValues
+        Byte::class -> mapMathResults(byteValues, Int::toByte)
+        Char::class -> mapMathResults(charValues, Int::toChar)
         Double::class -> doubleValues
         Float::class -> floatValues
         Int::class -> intValues
@@ -58,20 +60,20 @@ inline fun <reified S> getSumValues(): List<Pair<List<S>, S>> {
 
 @Suppress(Suppressions.UNCHECKED_CAST)
 inline fun <reified S> getProductValues(): List<Pair<List<S>, S>> {
-    val byteValues: List<Pair<List<Byte>, Byte>> = listOf(
-        emptyList<Byte>() to 0,
-        listOf(1).map(Int::toByte) to 1,
-        listOf(-1).map(Int::toByte) to -1,
-        listOf(5, 5, -2, 0).map(Int::toByte) to 0,
-        listOf(-3, -7, 2, 2).map(Int::toByte) to 84,
-        listOf(-3, -7, -2, 2).map(Int::toByte) to -84,
+    val byteValues = listOf(
+        emptyList<Int>() to 0,
+        listOf(1) to 1,
+        listOf(-1) to -1,
+        listOf(5, 5, -2, 0) to 0,
+        listOf(-3, -7, 2, 2) to 84,
+        listOf(-3, -7, -2, 2) to -84,
     )
-    val charValues: List<Pair<List<Char>, Char>> = listOf(
-        emptyList<Char>() to Char(0),
-        listOf(Char(0)) to Char(0),
-        listOf(Char(1)) to Char(1),
-        listOf(4, 4, 0).map(Int::toChar) to Char(0),
-        listOf(15, 23, 4, 4).map(Int::toChar) to Char(5520)
+    val charValues = listOf(
+        emptyList<Int>() to 0,
+        listOf(0) to 0,
+        listOf(1) to 1,
+        listOf(4, 4, 0) to 0,
+        listOf(15, 23, 4, 4) to 5520,
     )
     val doubleValues: List<Pair<List<Double>, Double>> = listOf()
     val floatValues: List<Pair<List<Float>, Float>> = listOf()
@@ -79,8 +81,8 @@ inline fun <reified S> getProductValues(): List<Pair<List<S>, S>> {
     val longValues: List<Pair<List<Long>, Long>> = listOf()
     val shortValues: List<Pair<List<Short>, Short>> = listOf()
     return when (S::class) {
-        Byte::class -> byteValues
-        Char::class -> charValues
+        Byte::class -> mapMathResults(byteValues, Int::toByte)
+        Char::class -> mapMathResults(charValues, Int::toChar)
         Double::class -> doubleValues
         Float::class -> floatValues
         Int::class -> intValues
@@ -88,4 +90,8 @@ inline fun <reified S> getProductValues(): List<Pair<List<S>, S>> {
         Short::class -> shortValues
         else -> emptyList()
     } as List<Pair<List<S>, S>>
+}
+
+fun <T> mapMathResults(intValues: List<Pair<List<Int>, Int>>, fromInt: (Int) -> T): List<Pair<List<T>, T>> {
+    return intValues.map { it.first.map(fromInt) to fromInt(it.second) }
 }
