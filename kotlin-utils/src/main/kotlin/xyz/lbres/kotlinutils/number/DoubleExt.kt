9975457dee704a94405fd6de3664cbce1f6653b7
lbressler13 <lbressler13@gmail.com>
2026-06-27 20:03:28 -0400

    isWholeNumber for float and double (#87)

--- kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/number/DoubleExt.kt
+++ kotlin-utils/src/main/kotlin/xyz/lbres/kotlinutils/number/DoubleExt.kt
@@ -26,6 +26,13 @@ fun Double.isZero(): Boolean = abs(this) == 0.0
 fun Double.isNegative(): Boolean = this < -0.0
 
 /**
+ * Unary check to determine if value is a whole number
+ *
+ * @return [Boolean]: true if value is a whole number, false otherwise
+ */
+fun Double.isWholeNumber(): Boolean = rem(1).isZero()
+
+/**
  * Unary check to determine if value is null or zero
  *
  * @return [Boolean]: true if the value is null or zero, or false otherwise
